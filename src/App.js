8a4bb55c5a2a3359151e132dc9fbbdd6a423c414
fatimajcm <fatimamacias500@outlook.com>
2022-12-05 16:25:24 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <h2> Hola puedes editar algo aki abajo</h2>
+        <h2> nuevo cambio</h2>
+        <h2> nuevo cambio</h2>
+        <h2> nuevo cambio</h2>
+        <h2> nuevo cambio</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
