9c77de1bbe58f6371568e8e0c96be046f7a5a394
fatimajcm <fatimamacias500@outlook.com>
2022-12-05 16:13:11 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h2> Hola puedes editar algo aki abajo</h2>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
